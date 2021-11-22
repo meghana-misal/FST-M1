4c190bc039b45357ba230ee8134b09761a9f6652
meghana-misal <meghanavmisal@gmail.com>
2021-11-22 14:47:22 +0530

    Add files via upload

---
+++ Python/Activities/activity21.py
@@ -0,0 +1,17 @@
+import pytest
+
+def test_sum():
+    s = 3 + 2
+    assert s == 5
+
+def test_difference():
+    d = 20-10
+    assert d == 10
+
+def test_product():
+    p = 5*5
+    assert p == 25
+
+def test_division():
+    div = 25 / 5
+    assert div == 5
